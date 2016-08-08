b01f09d2e2db236f8beaece7e0cefd108ca95096
Kevin Russell <kevinrussell@Kevins-MacBook-Air.local>
2016-08-08 11:59:12 -0700

    added if else statement to main.js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,11 @@
-console.log("JS file is connected to HTML! Woo!")
+
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.")
+}
