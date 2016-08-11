6d33392051a23307650877e46f1bdb8e75c193b3
Kevin Russell <kevinrussell@Kevins-MacBook-Air.local>
2016-08-10 19:00:51 -0700

    editted html and js files for memory game

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,8 +4,23 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour) {
+/* if (cardTwo === cardFour) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.")
-}
+} 
+*/
+
+var gameBoard = document.getElementById('game-board');
+
+var createBoard = function() {
+	for (var i = 0; i < gameBoard.length; i += 1) {
+		var cardElement = document.createElement('div');
+		cardElement.className('card');
+		gameBoard.appendChild(cardElement);
+	}
+};
+
+
+
+
